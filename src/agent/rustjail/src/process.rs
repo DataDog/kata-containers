bb3e63de19db0fd1c0a1a353b1de92f7fd64b2e9
Eric Mountain <eric.mountain@datadoghq.com>
2025-08-21 13:36:46 +0200

    Map stdin to /dev/null for non-interactive exec
    Signed-off-by: Eric Mountain <eric.mountain@datadoghq.com>

--- src/agent/rustjail/src/process.rs
+++ src/agent/rustjail/src/process.rs
@@ -175,9 +175,12 @@ impl Process {
             } else {
                 info!(logger, "created console socket!");
 
-                let (stdin, pstdin) = unistd::pipe2(OFlag::O_CLOEXEC)?;
-                p.parent_stdin = Some(pstdin);
-                p.stdin = Some(stdin);
+                let devnull = std::fs::OpenOptions::new()
+                    .read(true)
+                    .open("/dev/null")
+                    .map_err(|e| Errno::from_i32(e.raw_os_error().unwrap_or(libc::EIO)))?;
+                p.stdin = Some(devnull.as_raw_fd());
+                p.extra_files.push(devnull);
 
                 // These pipes are necessary as the stdout/stderr of the child process
                 // cannot be a socket. Otherwise, some images relying on the /dev/stdout(stderr)
