45d0109bd3b1ef35952b26954c5aa20576cca82f
Eric Mountain <eric.mountain@datadoghq.com>
2025-11-12 14:51:06 +0100

    exclude qgsport from factory config comparison

--- src/runtime/virtcontainers/factory/factory_linux.go
+++ src/runtime/virtcontainers/factory/factory_linux.go
@@ -80,6 +80,7 @@ func resetHypervisorConfig(config *vc.VMConfig) {
 	config.HypervisorConfig.SharedPath = ""
 	config.HypervisorConfig.VMStorePath = ""
 	config.HypervisorConfig.RunStorePath = ""
+	config.HypervisorConfig.QgsPort = 0
 }
 
 // It's important that baseConfig and newConfig are passed by value!
