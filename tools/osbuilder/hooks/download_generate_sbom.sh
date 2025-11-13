df80298b6730e4ae8f6f1bce640d1355fb535d0d
Eric Mountain <eric.mountain@datadoghq.com>
2025-11-13 14:36:57 +0100

    rm sbom.cdx.gz b4 gzip
    … in case it’s already there (but why is it?)

--- tools/osbuilder/hooks/download_generate_sbom.sh
+++ tools/osbuilder/hooks/download_generate_sbom.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eux
 
 trivy fs /rootfs -f cyclonedx -o /kata-containers/sbom.cdx 2>&1 | tee /kata-containers/trivy.log
-
+rm  /kata-containers/sbom.cdx.gz
 gzip /kata-containers/sbom.cdx
