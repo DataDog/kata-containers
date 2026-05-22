5aaeb69c8b264daf0f8f14fa49c3a0b6e70d2164
Mateo Lelong <mateo.lelong@datadoghq.com>
2026-05-22 11:17:02 +0200

    [datadog] chore: sbom generation

--- tools/osbuilder/rootfs-builder/ubuntu/rootfs_lib.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/rootfs_lib.sh
@@ -13,6 +13,7 @@ build_rootfs() {
 	# This fixes the spurious error
 	# E: Can't find a source to download version '2021.03.26' of 'ubuntu-keyring:amd64'
 	apt update
+
 	# focal version of mmdebstrap only supports comma separated package lists
 	# shellcheck disable=SC2154
 	if [[ "${OS_VERSION}" = "focal" ]]; then
@@ -22,6 +23,7 @@ build_rootfs() {
 	# shellcheck disable=SC2154
 	if ! mmdebstrap --mode auto --arch "${DEB_ARCH}" --variant required \
 			--components="${REPO_COMPONENTS}" \
+			--customize-hook "/kata-containers/tools/osbuilder/hooks/download_generate_sbom.sh" \
 			--include "${PACKAGES},${EXTRA_PKGS}" "${OS_VERSION}" "${rootfs_dir}" "${REPO_URL}"; then
 		echo "ERROR: mmdebstrap failed, cannot proceed" && exit 1
 	else
